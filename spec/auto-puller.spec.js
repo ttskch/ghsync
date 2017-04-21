de76f3827e95546be4624737a49151876a2f4a4e
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2017-04-21 16:23:02 +0900

    wip

--- spec/auto-puller.spec.js
+++ spec/auto-puller.spec.js
@@ -21,8 +21,9 @@ describe('autoPull()', function () {
                 if (property === 'repos') {
                     return [
                         { remote: 'remote1', local: 'local1' },
-                        { remote: 'remote2', local: 'local2' },
-                        { remote: 'remote3', local: 'local3' }
+                        { remote: 'remote2', local: 'local2', postPullCmd: 'single command' },
+                        { remote: 'remote3', local: 'local3', postPullCmd: ['first command', 'second command'] },
+                        { remote: 'remote4', local: 'local4', postPullCmd: null }
                     ];
                 }
             }
@@ -89,7 +90,7 @@ describe('autoPull()', function () {
 
     it('should call email notifier when git-pull command fail', function () {
 
-        // to succeed.
+        // to occur error.
         createSpyExec(false);
 
         var event = {
@@ -126,6 +127,174 @@ describe('autoPull()', function () {
 
         expect(console.log).not.toHaveBeenCalledWith('called');
     });
+
+    it('should call single postPullCmd', function () {
+
+        // to succeed.
+        createSpyExec(true);
+
+        // to succeed.
+        var spyExecPostPull = createSpyExecPostPull(true);
+
+        var event = {
+            payload: {
+                repository: {
+                    full_name: 'remote2'
+                }
+            }
+        };
+
+        autoPull(event);
+
+        expect(spyExecPostPull).toHaveBeenCalledWith('single command', {cwd: 'local2'});
+    });
+
+    it('should call multiple postPullCmd', function () {
+
+        // to succeed.
+        createSpyExec(true);
+
+        // to succeed.
+        var spyExecPostPull = createSpyExecPostPull(true);
+
+        var event = {
+            payload: {
+                repository: {
+                    full_name: 'remote3'
+                }
+            }
+        };
+
+        autoPull(event);
+
+        expect(spyExecPostPull).toHaveBeenCalledTimes(2);
+        expect(spyExecPostPull).toHaveBeenCalledWith('first command', {cwd: 'local3'});
+        expect(spyExecPostPull).toHaveBeenCalledWith('second command', {cwd: 'local3'});
+    });
+
+    it('should not call execPostPull when postPullCmd is not defined or set to null', function () {
+
+        // to succeed.
+        createSpyExec(true);
+
+        var spyExecPostPull = createSpyExecPostPull();
+
+        var event = {
+            payload: {
+                repository: {
+                    full_name: 'remote1'
+                }
+            }
+        };
+
+        autoPull(event);
+
+        expect(spyExecPostPull).not.toHaveBeenCalled();
+
+        event = {
+            payload: {
+                repository: {
+                    full_name: 'remote4'
+                }
+            }
+        };
+
+        autoPull(event);
+
+        expect(spyExecPostPull).not.toHaveBeenCalled();
+    });
+
+    it('should call email notifier when postPullCmd fail', function () {
+
+        // to succeed.
+        createSpyExec(true);
+
+        // to occur error.
+        createSpyExecPostPull(false);
+
+        var event = {
+            payload: {
+                repository: {
+                    full_name: 'remote2'
+                }
+            }
+        };
+
+        spyOn(console, 'log');
+
+        autoPull(event);
+
+        expect(console.log).toHaveBeenCalledWith('called');
+    });
+
+    it('should not call email notifier when postPullCmd succeed', function () {
+
+        // to succeed.
+        createSpyExec(true);
+
+        // to succeed.
+        createSpyExecPostPull(true);
+
+        var event = {
+            payload: {
+                repository: {
+                    full_name: 'remote2'
+                }
+            }
+        };
+
+        spyOn(console, 'log');
+
+        autoPull(event);
+
+        expect(console.log).not.toHaveBeenCalledWith('called');
+    });
+
+    // it('should turn global.hasError false after succeed postPullCmd', function () {
+    //
+    //     // to succeed.
+    //     createSpyExec(true);
+    //
+    //     // to succeed.
+    //     createSpyExecPostPull(true);
+    //
+    //     var event = {
+    //         payload: {
+    //             repository: {
+    //                 full_name: 'remote2'
+    //             }
+    //         }
+    //     };
+    //
+    //     global.hasError = true;
+    //
+    //     autoPull(event);
+    //
+    //     expect(global.hasError).toBe(false);
+    // });
+    //
+    // it('should turn global.hasError true after fail postPullCmd', function () {
+    //
+    //     // to occur error.
+    //     createSpyExec(false);
+    //
+    //     // to occur error.
+    //     createSpyExecPostPull(false);
+    //
+    //     var event = {
+    //         payload: {
+    //             repository: {
+    //                 full_name: 'remote2'
+    //             }
+    //         }
+    //     };
+    //
+    //     global.hasError = false;
+    //
+    //     autoPull(event);
+    //
+    //     expect(global.hasError).toBe(true);
+    // });
 });
 
 function createSpyExec(toSucceed) {
@@ -137,6 +306,19 @@ function createSpyExec(toSucceed) {
     };
 
     autoPuller.__set__('exec', mockExec);
+
+    return spy;
+}
+
+function createSpyExecPostPull(toSucceed) {
+    var spy = jasmine.createSpy('exec');
+
+    var mockExec = function (cmd, options, callback) {
+        spy(cmd, options);
+        callback(toSucceed ? null : 'An error occurred', 'stdout', 'stderr');
+    };
+
+    autoPuller.__set__('execPostPull', mockExec);
 
     return spy;
 }
--- src/auto-puller.js
+++ src/auto-puller.js
@@ -2,6 +2,7 @@
 
 var config = require('config');
 var exec = require('child_process').exec;
+var execPostPull = require('child_process').exec;
 var notifier = require('./notifier');
 
 module.exports.autoPull = function (event) {
@@ -17,6 +18,25 @@ module.exports.autoPull = function (event) {
                     notifier.sendmail(repo.local, stdout, stderr);
                 } else {
                     global.hasError = false;
+
+                    // postPullCmd.
+                    if (!repo.postPullCmd) {
+                        return;
+                    }
+                    var postPulls = Array.isArray(repo.postPullCmd) ? repo.postPullCmd : [repo.postPullCmd];
+                    postPulls.forEach(function (cmd) {
+                        console.log(cmd, repo.local);
+                        execPostPull(cmd, {cwd: repo.local}, function (err, stdout, stderr) {
+                            console.log(stdout);
+                            if (err) {
+                                global.hasError = true;
+                                console.log(stderr);
+                                notifier.sendmail(repo.local, stdout, stderr);
+                            } else {
+                                global.hasError = false;
+                            }
+                        });
+                    });
                 }
             });
         }
